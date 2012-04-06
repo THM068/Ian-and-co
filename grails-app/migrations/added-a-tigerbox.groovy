acf51352cba29b55b5fa77e70eca60025c699409
thando <thandomafela@hotmail.com>
2012-04-06 21:24:27 +0000

    fixd migration file

--- grails-app/migrations/added-a-tigerbox.groovy
+++ grails-app/migrations/added-a-tigerbox.groovy
@@ -14,19 +14,4 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "thandomafela (generated)", id: "1333314853978-2") {
-		createTable(tableName: "tiger_box_photo") {
-			column(name: "tiger_box_photos_id", type: "BIGINT")
-
-			column(name: "photo_id", type: "BIGINT")
-		}
-	}
-
-	changeSet(author: "thandomafela (generated)", id: "1333314853978-3") {
-		addForeignKeyConstraint(baseColumnNames: "photo_id", baseTableName: "tiger_box_photo", constraintName: "FK9182A33EF089A326", deferrable: "false", initiallyDeferred: "false", onDelete: "NO ACTION", onUpdate: "NO ACTION", referencedColumnNames: "id", referencedTableName: "photo", referencesUniqueColumn: "false")
-	}
-
-	changeSet(author: "thandomafela (generated)", id: "1333314853978-4") {
-		addForeignKeyConstraint(baseColumnNames: "tiger_box_photos_id", baseTableName: "tiger_box_photo", constraintName: "FK9182A33EE8EAF05", deferrable: "false", initiallyDeferred: "false", onDelete: "NO ACTION", onUpdate: "NO ACTION", referencedColumnNames: "id", referencedTableName: "tiger_box",  referencesUniqueColumn: "false")
-	}
 }
